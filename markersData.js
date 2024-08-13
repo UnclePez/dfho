f488f95043652a9df03be5858b00809b3fbaf19f
Ezra DeMaere <demaere.ezra@gmail.com>
2024-08-13 15:40:35 -0600

    Added icons, made separate data files for markers, adjusted hover functionality

---
+++ markersData.js
@@ -0,0 +1,76 @@
+// MARKER DATA ------------------------------------------------
+const markersData = [
+    {
+        coordinates: [2600, 1910],
+        iconUrl: 'images/icons/hacker-computer-icon.png',
+        tooltipText: 'HACKER COMPUTER',
+        popupText: 'This will show information about the Hacker Computer'
+    },
+    {
+        coordinates: [3932, 1188],
+        iconUrl: 'images/icons/hacker-computer-icon.png',
+        tooltipText: 'HACKER COMPUTER',
+        popupText: 'This will show information about the Hacker Computer'
+    },
+    {
+        coordinates: [4262, 1227],
+        iconUrl: 'images/icons/safe-icon.png',
+        tooltipText: 'SAFE',
+        popupText: 'This will show information about the Safe'
+    },
+    {
+        coordinates: [4207, 1374],
+        iconUrl: 'images/icons/boss-icon.png',
+        tooltipText: 'BOSS',
+        popupText: 'This will show information about the Boss'
+    },
+    {
+        coordinates: [3952, 1100],
+        iconUrl: 'images/icons/event-trigger-icon.png',
+        tooltipText: 'INDUSTRIAL ELEVATOR SWITCH',
+        popupText: 'This will show information about the Industrial Elevator Switch'
+    },
+    {
+        coordinates: [4660, 1990],
+        iconUrl: 'images/icons/event-trigger-icon.png',
+        tooltipText: 'INDUSTRIAL ELEVATOR SWITCH',
+        popupText: 'This will show information about the Industrial Elevator Switch'
+    },
+    {
+        coordinates: [4695, 685],
+        iconUrl: 'images/icons/industrial-extract-icon.png',
+        tooltipText: 'INDUSTRIAL ELEVATOR EXTRACT',
+        popupText: 'This will show information about the Industrial Elevator Extract'
+    },
+    {
+        coordinates: [4304, 2446],
+        iconUrl: 'images/icons/safe-icon.png',
+        tooltipText: 'SAFE',
+        popupText: 'This will show information about the Safe'
+    },
+    {
+        coordinates: [3494, 2841],
+        iconUrl: 'images/icons/paid-extract-icon.png',
+        tooltipText: 'PAID EXTRACT',
+        popupText: 'This will show information about the Paid Extract'
+    },
+    {
+        coordinates: [5264, 4151],
+        iconUrl: 'images/icons/random-extract-icon.png',
+        tooltipText: 'RANDOM EXTRACT',
+        popupText: 'This will show information about the Random Extract'
+    },
+    {
+        coordinates: [5815, 1485],
+        iconUrl: 'images/icons/conditional-extract-icon.png',
+        tooltipText: 'CONDITIONAL EXTRACT',
+        popupText: 'This will show information about the Conditional Extract'
+    },
+    {
+        coordinates: [5920, 2910],
+        iconUrl: 'images/icons/extract-icon.png',
+        tooltipText: 'EXTRACT',
+        popupText: 'This will show information about the Extract'
+    },
+    // Add more markers here
+];
